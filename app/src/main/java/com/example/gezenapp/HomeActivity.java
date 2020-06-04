a1e9551236ee0e5b34e6c744c5fbd3014590729d
realka <karaaliidris@gmail.com>
2020-06-04 22:09:18 +0300

    Nav eklendi ama clickler eksik, detay görüntü eksik, resim eksik.

--- app/src/main/java/com/example/gezenapp/HomeActivity.java
+++ app/src/main/java/com/example/gezenapp/HomeActivity.java
@@ -4,15 +4,20 @@ package com.example.gezenapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -34,7 +39,8 @@ public class HomeActivity extends AppCompatActivity implements MyRecyclerViewAda
     RecyclerView recyclerView;
     MyRecyclerViewAdapter adapter;
 
-    Button cikis;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,32 +54,18 @@ public class HomeActivity extends AppCompatActivity implements MyRecyclerViewAda
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
 
                 for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
                     Post post = postSnapshot.getValue(Post.class);
                     posts.add(post);
-                    // here you can access to name property like university.name
 
                 }
 
                 recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
                 recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
                 adapter = new MyRecyclerViewAdapter(recyclerView.getContext(), posts);
-                //adapter.setClickListener(recyclerView.getContext());
                 adapter.setClickListener((MyRecyclerViewAdapter.ItemClickListener) recyclerView.getContext());
                 recyclerView.setAdapter(adapter);
-
-                /*mAdapter = new CustomAdepter(getBaseContext(), posts);
-                for (int i =0; i < posts.size(); i++){
-                    recyclerView.setAdapter(mAdapter.getView(i,recyclerView,null));
-                }*/
-                //recyclerView.setAdapter(mAdapter);
-                /*for (int i =0; i < posts.size(); i++){
-                    header.setText(posts.get(i).getHeader());
-                    abs.setText(posts.get(i).getContext());
-                }*/
 
             }
 
@@ -85,20 +77,52 @@ public class HomeActivity extends AppCompatActivity implements MyRecyclerViewAda
         });
 
 
-        cikis = findViewById(R.id.cikisyap);
-        cikis.setOnClickListener(new View.OnClickListener() {
+        //Navigation
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.Open,R.string.Close);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent iMain = new Intent(HomeActivity.this, MainActivity.class);
-                startActivity(iMain);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.cikisyap){
+
+                     FirebaseAuth.getInstance().signOut();
+                     Intent iMain = new Intent(HomeActivity.this, MainActivity.class);
+                      startActivity(iMain);
+
+                }else  if (id == R.id.home){
+
+                    Toast.makeText(HomeActivity.this,"Anasayfa",Toast.LENGTH_SHORT);
+
+                }else if (id == R.id.profile){
+
+                    Toast.makeText(HomeActivity.this,"Profile",Toast.LENGTH_SHORT);
+
+                }
+                return true;
             }
-        });
+        })
+        ;
+
 
     }
 
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return actionBarDrawerToggle.onOptionsItemSelected(item) ||  super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onItemClick(View view, int position) {
-        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 }
